cf09aa12e286900b13d1eabe9c4245772c656cfc
Nadia89M <nadia.mohamed89@gmail.com>
2019-02-01 11:36:39 +0000

    Template Driven Form

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
-import { flattenStyles } from '@angular/platform-browser/src/dom/dom_renderer';
-import { getMaxListeners } from 'cluster';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { User } from '../../models/user'
 
 @Component({
   selector: 'app-users',
@@ -11,69 +9,64 @@ import { getMaxListeners } from 'cluster';
 
 export class UsersComponent implements OnInit {
   user: User = {
-    firstName: "",
-    lastName: "",
-    email: ""
+    firstName: '',
+    lastName: '',
+    email: ''
   }
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = false;
   showUserForm: boolean = false;
+  @ViewChild('userForm') form: any;
 
   constructor() { }
 
   ngOnInit() {
+   
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'john@gmail.com',
+          isActive: true,
+          registered: new Date('01/02/2018 08:30:00'),
+          hide: true
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          email: 'kevin@yahoo.com',
+          isActive: false,
+          registered: new Date('03/11/2017 06:20:00'),
+          hide: true
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          email: 'karen@gmaial.com',
+          isActive: true,
+          registered: new Date('11/02/2016 10:30:00'),
+          hide: true
+        }
+      ];
 
-
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john@gmail.com',
-        isActive: true,
-        registered: new Date('01/03/2019 08:30:00'),
-        hide: true
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Doe',
-        email: 'kevin@gmail.com',
-        isActive: false,
-        hide: true,
-        registered: new Date('05/03/2019 08:30:00')
-      },
-      {
-        firstName: 'Karl',
-        lastName: 'Doe',
-        email: 'karl@gmail.com',
-        isActive: true,
-        hide: true,
-        registered: new Date('07/03/2019 08:30:00')
-      }
-    ];
-
-    this.loaded = true;
-
-   }
-
-  //  addUser(){
-  //    this.user.isActive = true;
-  //    this.user.registered = new Date;
-  //    this.users.unshift(this.user);
-  //    this.user = {
-  //     firstName: "",
-  //     lastName: "",
-  //     email: ""
-  //    }
-  //  }
-
-  //  toggleHide(user:User){
-  //    user.hide = !user.hide;
-  //  }
-
-  onSubmit(e){
+      this.loaded = true;
   }
 
-}
+  onSubmit({value, valid}: {value: User, valid: boolean}) {
+    if(!valid){
+      console.log('Form is not valid');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
 
+      this.users.unshift(value);
+      
+      this.form.reset();
+    }
+  }
+
+  
+}
